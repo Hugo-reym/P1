d9723e5317d0fe1aa06b471676f6abea68c47b63
Hugo-reym <montoliu.lupo@gmail.com>
2025-02-25 11:15:03 +0100

    Update dynamic_list.h

--- P1/dynamic_list.h
+++ P1/dynamic_list.h
@@ -11,7 +11,19 @@
 #define DYNAMIC_LIST_H
 
 #include "types.h"
-
-/*  Write your code here...  */
+#include <stdbool.h>
+#define LNULL NULL
+typedef int tItemL;
+typedef struct tNode *tPosL;
+struct tNode {
+  tItemL data;
+  tPosL next;
+}
+typedef tPosL tList;
+void CreateEmptyList(tList *L);
+bool CreateNode(tPosL p);
+bool insertItem(tItemL d, tPosL p, tList *L);
+bool CopyList(tList L, tList *M);
+bool CopyListR(tList L, tList *M); //This is recursive
 
 #endif
